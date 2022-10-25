ce6697b91201eaf71a68cffc96a7627b6beb66b4
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-10-25 23:46:22 +0600

    course name added

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 const express = require('express')
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
+
+app.use(cors());
+
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
     res.send('MecEdu API Running')
 });
+app.get('/all-courses', (req, res) => {
+    res.send(courses)
+})
 app.listen(port, () => {
     console.log('MecEdu Server running on port: ', port)
 })

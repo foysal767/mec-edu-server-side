eb3470e172b13fd3213a86856b9bbaeb4e154403
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-10-26 07:17:04 +0600

    course details function added

--- index.js
+++ index.js
@@ -5,13 +5,17 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const courses = require('./data/courses.json')
+const courses = require('./data/courses.json');
+const coursesDetails = require('./data/course-details.json');
 
 app.get('/', (req, res) => {
     res.send('MecEdu API Running')
 });
 app.get('/all-courses', (req, res) => {
     res.send(courses)
+})
+app.get('/courses-details', (req, res) => {
+    res.send(coursesDetails)
 })
 app.listen(port, () => {
     console.log('MecEdu Server running on port: ', port)

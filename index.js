d2380938f66f6c903b6f4fbd82a0f9cd9dfaafc9
foysal767 <uddinmohammadfoysal981@gmail.com>
2022-10-26 14:17:45 +0600

    category course api creating

--- index.js
+++ index.js
@@ -17,6 +17,16 @@ app.get('/all-courses', (req, res) => {
 app.get('/courses-details', (req, res) => {
     res.send(coursesDetails)
 })
+app.get('/course-category/:id', (req, res) => {
+    const id = req.params.id
+    const courseCategory = coursesDetails.find(n => n.category_id === id);
+    res.send(courseCategory);
+})
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = coursesDetails.find(n => n._id === id);
+    res.send(selectedCourse)
+})
 app.listen(port, () => {
     console.log('MecEdu Server running on port: ', port)
 })
